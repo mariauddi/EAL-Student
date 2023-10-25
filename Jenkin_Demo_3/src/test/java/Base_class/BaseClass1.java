e70507858a478b1595952f3d6398b9398807324e
muddi <muddi@10.0.0.174>
2023-10-24 21:15:36 -0400

    hello

--- Jenkin_Demo_3/src/test/java/Base_class/BaseClass1.java
+++ Jenkin_Demo_3/src/test/java/Base_class/BaseClass1.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
 import java.util.concurrent.TimeUnit;
@@ -13,6 +14,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 
 public class BaseClass1 {
 	WebDriver driver;
@@ -22,7 +24,7 @@ public class BaseClass1 {
 	
 	
   @Parameters({"browser","website"})
-  @BeforeClass
+  @BeforeMethod
   public void ibrowser(String mainofbrowser,String url) {
 		//String ud = System.getProperty("user.dir");
 		if(mainofbrowser.equalsIgnoreCase("chrome")) {
@@ -46,7 +48,7 @@ public class BaseClass1 {
   
   
   
-  @AfterClass
+  @AfterMethod
   public void afterClass() {
 	  driver.close();
   }
